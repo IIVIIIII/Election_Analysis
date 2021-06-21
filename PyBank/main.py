070e008874bc20177430dd479c5e574b98c0877c
IIVIIIII <kelwalls@gmail.com>
2021-06-21 12:58:37 -0700

    reformat text

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,10 @@
 import os
 import csv
 
+
 # set paths for csv to read and txt to write
-read_path = os.path.join("resources", "budget_data.csv")
-write_path = os.path.join("analysis", "budget_analysis.txt")
+read_path = os.path.join('resources', 'budget_data.csv')
+write_path = os.path.join('analysis', 'budget_analysis.txt')
 
 
 # set variables for results to calculate
@@ -34,28 +35,29 @@ with open(read_path, 'r') as bank_data:
             greatdecrease_n = int(row[1])
             greatdecrease_m = row[0]
 
-    average = "${:.2f}".format(float(total)/monthcount)
+    average = '${:.2f}'.format(float(total)/monthcount)
 
 
 # write results of analysis to a txt file
 analysis = open(write_path, 'w')
-analysis.write("Financial Analysis\n")
-analysis.write("----------------------------\n")
-analysis.write("Total Months: " + str(monthcount) + "\n")
-analysis.write("Total: $" + str(total) + "\n")
-analysis.write("Average Change: " + str(average) + "\n")
-analysis.write("Greatest Increase in Profits: " + str(greatincrease_m) + " ($" + str(greatincrease_n) + ")\n")
-analysis.write("Greatest Decrease in Profits: " + str(greatdecrease_m) + " ($" + str(greatdecrease_n) + ")\n")
+analysis.write('Financial Analysis\n')
+analysis.write('----------------------------\n')
+analysis.write(f'Total Months: {monthcount}\n')
+analysis.write(f'Total: ${total}\n')
+analysis.write(f'Average Change: {average}\n')
+analysis.write(f'Greatest Increase in Profits: {greatincrease_m} (${greatincrease_n})\n')
+analysis.write(f'Greatest Decrease in Profits: {greatdecrease_m} (${greatdecrease_n})\n')
 analysis.close
 
+
 # print results of analysis to terminal
-print("\nFinancial Analysis")
-print("----------------------------")
-print("Total Months: " + str(monthcount))
-print("Total: $" + str(total))
-print("Average Change: " + str(average))
-print("Greatest Increase in Profits: " + str(greatincrease_m) + " ($" + str(greatincrease_n) + ")")
-print("Greatest Decrease in Profits: " + str(greatdecrease_m) + " ($" + str(greatdecrease_n) + ")\n")
+print('\nFinancial Analysis')
+print('----------------------------')
+print(f'Total Months: {monthcount}')
+print(f'Total: ${total}')
+print(f'Average Change: {average}')
+print(f'Greatest Increase in Profits: {greatincrease_m} (${greatincrease_n})')
+print(f'Greatest Decrease in Profits: {greatdecrease_m} (${greatdecrease_n})\n')
 
 
 
--- PyPoll/main copy.py
+++ PyPoll/main copy.py
@@ -1,15 +1,18 @@
 import os
 import csv
 
+
 # set paths for csv to read and txt to write
-read_path = os.path.join("resources", "election_data.csv")
-write_path = os.path.join("analysis", "election_analysis.txt")
+read_path = os.path.join('resources', 'election_data.csv')
+write_path = os.path.join('analysis', 'election_analysis.txt')
+
 
 # set variables for results to calculate
 results = {}
 total = 0
 high = 0
 winner = ""
+
 
 # read csv and analyise data
 with open(read_path, 'r') as election_data:
@@ -20,15 +23,13 @@ with open(read_path, 'r') as election_data:
 
     for row in election_read:
         total = total + 1
-
         if row[2] in results:
             results[row[2]]['votes'] = results[row[2]]['votes'] + 1
         else:
             results[row[2]] = {'votes': 1, 'percentage': 0}
 
     for canidate in results.keys():
-        results[canidate]['percentage'] = "{:.2%}".format(results[canidate]['votes']/total)
-
+        results[canidate]['percentage'] = '{:.2%}'.format(results[canidate]['votes']/total)
 
 
 # write results of analysis to a txt file
@@ -39,11 +40,10 @@ analysis.write(f'Total Votes: {total}\n')
 analysis.write('-------------------------\n')
 
 for result in results:
-    analysis.write(f'{result}: {results[result]["percentage"]} ({results[result]["votes"]})\n')
-    
     if results[result]['votes'] > high:
         winner = result
         high = results[result]['votes']
+    analysis.write(f'{result}: {results[result]["percentage"]} ({results[result]["votes"]})\n')
 
 analysis.write("-------------------------\n")
 analysis.write(f'Winner: {winner}\n')
@@ -62,69 +62,3 @@ for result in results:
 print("-------------------------")
 print(f'Winner: {winner}')
 print('-------------------------\n')
-
-
-
-
-
-
-
-
-
-
-# with open(read_path, 'r') as election_data:
-
-#     election_read = csv.reader(election_data)
-
-#     next(election_read, None)
-
-#     for row in election_read:
-#         cans[row[2]]["Votes"] = cans[row[2]]["Votes"] + 1
-#     for can in cans:
-#         if cans[can]["Votes"] > high:
-#             high = cans[can]["Votes"]
-#             winner = can
-#         cans[can]["Percent"] = "{:.3%}".format(cans[can]["Votes"]/float(total))
-#         results.append(str(can)+": "+str(cans[can]["Percent"])+" ("+str(cans[can]["Votes"])+")")
-
-#     print("Election Results")
-#     print("-------------------------")
-#     print("Total Votes: "+str(total))
-#     print("-------------------------")
-#     for result in results:
-#         print(result)
-#     print("-------------------------")
-#     print("Winner: "+str(winner))
-#     print("-------------------------")
-
-# with open(write_path, 'w') as election_analysis:
-
-#     election_write = csv.writer(election_analysis, delimiter=',')
-
-#     election_write.writerow(["Election Results"])
-#     election_write.writerow(["-------------------------"])
-#     election_write.writerow(["Total Votes: "+str(total)])
-#     election_write.writerow(["-------------------------"])
-#     for result in results:
-#         election_write.writerow([result])
-#     election_write.writerow(["-------------------------"])
-#     election_write.writerow(["Winner: "+str(winner)])
-#     election_write.writerow(["-------------------------"])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
